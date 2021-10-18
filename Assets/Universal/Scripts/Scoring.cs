1d88362aa4e6496c3e5404275043d7c9d1f7e3cc
wixkedbeast <79669745+wixkedbeast@users.noreply.github.com>
2021-10-18 18:32:03 +1100

    Week 4 class commit

--- Assets/Universal/Scripts/Scoring.cs
+++ Assets/Universal/Scripts/Scoring.cs
@@ -1,21 +1,98 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Scoring : JMC
 {
     public int lastRoundScore = 100;
     public int thisRoundScore = 170;
     public int lives = 5;
-
     
-    
+    [Header("Score Based")]
+    public int highestScore;
+    public int currentScore;
+    public TMP_Text currentScoreText;
+    public TMP_Text highScoreText;
 
-    // Update is called once per frame
+    [Header("Time Based")]
+    public float bestTime;
+    public float currentTime;
+    public TMP_Text bestTimeText;
+    public TMP_Text currentTimeText;
+    public GameObject winPanel;
+    public TMP_Text bestTimeWinText;
+    public TMP_Text currentTimeWinText;
+    public TMP_Text winMessageText;
+
+    public List<GameObject> collectables;
+
+    float timer = 0;
+    bool isTiming = true;
+
+
+    private void Start()
+    {
+        winPanel.SetActive(false);
+        if (PlayerPrefs.HasKey("BestTime"))
+        {
+            bestTime = PlayerPrefs.GetFloat("BestTime");
+            bestTimeText.text = "BestTime: " + bestTime.ToString("F2");
+        }
+        else
+            bestTimeText.text = "Best Time: Not Yet Set";
+        bestTime = 10000000;
+        
+    }
     void Update()
     {
-        if (IsGameOver(lives))
-            CheckScore();
+        if (isTiming)
+            timer += Time.deltaTime;
+
+        currentTimeText.text = "Current Time: " + timer.ToString("F2");
+
+        if (Input.GetKeyDown(KeyCode.P))
+            DeletePrefs();
+
+    }
+
+    void DeletePrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        Collect(other.gameObject);
+    }
+
+    public void Collect(GameObject _go)
+    {
+        _go.SetActive(false);
+        collectables.Remove(_go);
+        if(collectables.Count == 0)
+        {
+            isTiming = false;
+
+        }
+    }
+
+    void GameOver()
+    {
+        isTiming = false;
+        currentTimeWinText.text = "Your Time: " + currentTime.ToString("F2");
+        if (currentTime <= bestTime)
+        {
+            bestTime = currentTime;
+            PlayerPrefs.SetFloat("BestTime", bestTime);
+            winMessageText.text = "Congratulations!!!\n You got a new best Time!!!";
+            bestTimeWinText.text = "BestTime: " + bestTime.ToString("F2");
+        }
+        else
+        {
+            winMessageText.text = "Loser!!!\n You got no skillz!!!";
+        }
         
     }
 
