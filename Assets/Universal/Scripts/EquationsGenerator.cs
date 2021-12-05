0d492036b23c8c1aceaa900ee7e69f533e834811
wixkedbeast <79669745+wixkedbeast@users.noreply.github.com>
2021-12-05 23:43:04 +1100

    Week 10 commit 2

--- Assets/Universal/Scripts/EquationsGenerator.cs
+++ Assets/Universal/Scripts/EquationsGenerator.cs
@@ -32,9 +32,27 @@ public class EquationsGenerator : Singleton<EquationsGenerator> {
     float roundSpeed = 7;
     bool playerCorrect;
     public GameObject messagePanel;
+    public float time = 5;
+    public float force = 50;
+    public float forceSide;
+    public float forceSide2;
+    public float forceSide3;
+    public Vector3 startPos;
+    public Vector3 startPos2;
+    public Vector3 startPos3;
+    public Quaternion rotationPos1;
+    public Quaternion rotationPos2;
+    public Quaternion rotationPos3;
 
     void Start()
     {
+        
+        startPos = plate1.transform.position;
+        startPos2 = plate2.transform.position;
+        startPos3 = plate3.transform.position;
+        rotationPos1 = plate1.transform.rotation;
+        rotationPos2 = plate2.transform.rotation;
+        rotationPos3 = plate3.transform.rotation;
         plate1Text = plate1.GetComponentInChildren<TMP_Text>();
         plate2Text = plate2.GetComponentInChildren<TMP_Text>();
         plate3Text = plate3.GetComponentInChildren<TMP_Text>();
@@ -172,12 +190,14 @@ public class EquationsGenerator : Singleton<EquationsGenerator> {
             ScoringSystem.instance.AddScore(myScore);
             
 
+
         }
         else
         {
             message.text = "Wrong!";
             message.color = Color.red;
             
+
         }
         
         
@@ -193,11 +213,20 @@ public class EquationsGenerator : Singleton<EquationsGenerator> {
     }
     IEnumerator LaunchPlates()
     {
+        forceSide = Random.Range(-400, 400);
+        //forceSide2 = Random.Range(-400, 400);
+        //forceSide3 = Random.Range(-400, 400);
         playerCorrect = false;
         GenerateRandomEquation();
         plate1.SetActive(true);
         plate2.SetActive(true);
         plate3.SetActive(true);
+        plate1.GetComponent<Rigidbody>().AddForce(transform.up * force);
+        plate1.GetComponent<Rigidbody>().AddForce(transform.right * forceSide);
+        plate2.GetComponent<Rigidbody>().AddForce(transform.up * force);
+        //plate2.GetComponent<Rigidbody>().AddForce(transform.right * forceSide2);
+        plate3.GetComponent<Rigidbody>().AddForce(transform.up * force);
+        //plate3.GetComponent<Rigidbody>().AddForce(transform.right * forceSide3);
         //Set plates to a random start pos 
         //add force in an upwards direction
         //round
@@ -205,9 +234,16 @@ public class EquationsGenerator : Singleton<EquationsGenerator> {
 
         CheckAnswer(playerCorrect);
         yield return new WaitForSeconds(2);
-        
+        message.text = "";
+        plate1.transform.position = startPos;
+        plate2.transform.position = startPos2;
+        plate3.transform.position = startPos3;
+        plate1.transform.rotation = rotationPos1;
+        plate2.transform.rotation = rotationPos2;
+        plate3.transform.rotation = rotationPos3;
+
         //answersField.text = "";
-        if(roundNumber != 10)
+        if (roundNumber != 10)
         {
             roundNumber++;
             StartCoroutine(LaunchPlates());
